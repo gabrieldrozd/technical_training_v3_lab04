53700f5523ba7415156f976cbffcb31ba372c727
Gabriel <drozd.gj@gmail.com>
2022-04-11 19:31:34 +0200

    final V

---
+++ lab04/ApplicationClient/Form1.cs
@@ -0,0 +1,70 @@
+﻿using CalcServiceLibrary;
+using MathServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApplicationClient
+{
+    public partial class Form1 : Form
+    {
+        //private readonly IMathService _mathClient = ChannelFactory<IMathService>.CreateChannel(
+        //    new BasicHttpBinding(),
+        //    new EndpointAddress("http://localhost:4444/MathService")
+        //    );
+        //private readonly ICalcService _calcClient = ChannelFactory<ICalcService>.CreateChannel(
+        //    new BasicHttpBinding(),
+        //    new EndpointAddress("http://localhost:5555/CalcService")
+        //    );
+        private readonly IMathService _mathClient = new ChannelFactory<IMathService>("MathService").CreateChannel();
+        private readonly ICalcService _calcClient = new ChannelFactory<ICalcService>("CalcService").CreateChannel();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+
+            int result = _mathClient.Add(new MathServiceLibrary.MyNumbers() { Number1 = number1, Number2 = number2 });
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+
+            int result = _mathClient.Substract(new MathServiceLibrary.MyNumbers() { Number1 = number1, Number2 = number2 });
+            textBox3.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+
+            int result = _calcClient.Multiply(new CalcServiceLibrary.MyNumbers() { Number1 = number1, Number2 = number2 });
+            textBox3.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+
+            int result = _calcClient.Divide(new CalcServiceLibrary.MyNumbers() { Number1 = number1, Number2 = number2 });
+            textBox3.Text = result.ToString();
+        }
+    }
+}
---
+++ lab04/SelfHostMathService/Program.cs
@@ -0,0 +1,72 @@
+﻿using CalcServiceLibrary;
+using MathServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHostMathService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceSelfHostMathLibrary = null, serviceSelfHostCalcLibrary = null;
+
+            try
+            {
+                serviceSelfHostMathLibrary = new ServiceHost(typeof(MathService));
+                serviceSelfHostCalcLibrary = new ServiceHost(typeof(CalcService));
+
+                //serviceSelfHostMathLibrary.AddServiceEndpoint(
+                //    typeof(IMathService),
+                //    new BasicHttpBinding(),
+                //    "http://localhost:4444/MathService");
+
+                //serviceSelfHostCalcLibrary.AddServiceEndpoint(
+                //    typeof(ICalcService),
+                //    new BasicHttpBinding(),
+                //    "http://localhost:5555/CalcService");
+
+                serviceSelfHostMathLibrary.Open();
+                serviceSelfHostCalcLibrary.Open();
+
+                Console.WriteLine("Hosting został uruchomiony");
+                Console.WriteLine("Serwisy nasłuchują żądań klientów na adresach:");
+                Console.WriteLine();
+
+                foreach (var serviceSelfHostEndpoint in serviceSelfHostMathLibrary.Description.Endpoints)
+                {
+                    Console.WriteLine($"Adres: {serviceSelfHostEndpoint.Address} Wiązanie: {serviceSelfHostEndpoint.Binding.Name}");
+                }
+
+                foreach (var serviceSelfHostEndpoint in serviceSelfHostCalcLibrary.Description.Endpoints)
+                {
+                    Console.WriteLine($"Adres: {serviceSelfHostEndpoint.Address} Wiązanie: {serviceSelfHostEndpoint.Binding.Name}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Wciśnięcie dowolnego klawisza kończy pracę");
+                Console.ReadKey();
+
+                serviceSelfHostMathLibrary.Close();
+                serviceSelfHostCalcLibrary.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            finally
+            {
+                if (serviceSelfHostMathLibrary != null && serviceSelfHostMathLibrary.State == CommunicationState.Opened) 
+                    serviceSelfHostMathLibrary.Close();
+                
+                if (serviceSelfHostCalcLibrary != null && serviceSelfHostCalcLibrary.State == CommunicationState.Opened) 
+                    serviceSelfHostCalcLibrary.Close();
+            }
+        }
+    }
+}
